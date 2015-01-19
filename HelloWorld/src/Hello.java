af388378d7303aaae1e379b2aae8cb67f1c8ea6c
narendra <narendrar@egnaroinc.com>
2015-01-19 11:04:32 +0530

    Changed Mistake things
    Signed-off-by: narendra <narendrar@egnaroinc.com>

--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -1,7 +1,7 @@
 
 public class Hello {
 
-	public static void maoin(String args[]){
+	public static void main(String args[]){
 		System.out.println("Testing hellow world");
 		System.out.println("Welcome to github");
 	}
